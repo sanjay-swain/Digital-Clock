e545ebbbed2a29742087ea620adbef9fbce603cd
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-27 22:26:56 +0530

    Modified monitor speed and added different library

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,58 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include "time.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_I2CDevice.h>
-#include "Adafruit_SH1106.h"
+#include <U8g2lib.h>
+#include <sstream>
 
+// OLED constructor
+// Please check on the link below and according to the display driver on your OLED change the line below:
+// https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, SCL, SDA); 
+
+// change the SSID and password according to your WiFi network
 const char* ssid      = "yourssid";
 const char* password  = "yourpassword";
 
 const char* ntpServer = "pool.ntp.org";
+// Change the data below according to your timezone and daylight offset (in seconds)
 const long  gmtOffset_sec = 19800;	// Time zone of IST in seconds
 const long 	daylightOffset_sec = 0;
 
+/*
+	NOTE: I have really low C++ experience so I can't really find a way to concanate strings with integers
+	as I have to convert integers into strings and then convert them into char* as the drawStr method
+	expects it. So I don't think the way I implemented it is an efficient solution but it works.
+*/
+std::string curr_time;
 
-Adafruit_SH1106 display(SDA, SCL);  // Using default SDA and SCL pins of ESP32.
+std::string IntToString(int a)
+{
+    std::ostringstream temp;
+    temp << a;
+    return temp.str();
+}
 
 void displayLocalTime() {
 	struct tm timeinfo;
 	if (!getLocalTime(&timeinfo)) {
 		Serial.println("Failed to obtain time");
-
-        display.clearDisplay();
-        display.setCursor(0,28);
-        display.println("Failed to obtain time");
-        display.display();
 		return;
 	}
 
-	display.clearDisplay();
-	display.setCursor(0,28);
-	display.println(&timeinfo, "%A, %B, %d, %Y, %H: %M: %S");
-	display.display();
-	Serial.println(&timeinfo, "%A, %B, %d, %Y, %H: %M: %S");
+	curr_time = IntToString(timeinfo.tm_hour) + ": " + IntToString(timeinfo.tm_min) + ": " + IntToString(timeinfo.tm_sec);
+	u8g2.clearBuffer();
+	u8g2.setFont(u8g2_font_ncenB14_tr);
+	u8g2.drawStr(0, 18, "Time:");
+	u8g2.drawStr(10, 50, curr_time.c_str());
+	u8g2.sendBuffer();
+
+	Serial.println(curr_time.c_str());
 }
 
 void setup() {
 	Serial.begin(115200);
 
-	display.begin(SH1106_SWITCHCAPVCC, 0x3C);
-
-	display.clearDisplay();
-
-	display.setTextSize(1);
-	display.setTextColor(WHITE);
+	u8g2.begin();
 
 	// Connect to WiFi
 	Serial.printf("Connecting to %s", ssid);
@@ -53,7 +63,7 @@ void setup() {
 	}
 	Serial.println("  CONNECTED");
 
-	// init and get the time
+	// get the time from internet
 	configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 	displayLocalTime();
 
@@ -63,6 +73,6 @@ void setup() {
 }
 
 void loop() {
-	delay(900);
 	displayLocalTime();
+	delay(900);
 }
